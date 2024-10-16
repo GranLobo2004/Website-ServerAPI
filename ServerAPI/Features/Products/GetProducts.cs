d4df3803f96b78cf5621308636793fb6e9da2eee
GranLobo2004 <jaimeochoadealdacerdan@outlook.es>
2024-10-16 18:45:10 +0300

    Adding funtionalities of filter to getproducts

--- ServerAPI/Features/Products/GetProducts.cs
+++ ServerAPI/Features/Products/GetProducts.cs
@@ -1,45 +1,92 @@
 ﻿using FastEndpoints;
 using Microsoft.EntityFrameworkCore;
 using ServerAPI.Data;
+using ServerAPI.Entities;
 
-namespace ServerAPI.Features
+namespace ServerAPI.Features;
+
+public record GetProductsRequest (string Name, int MinPrice, int MaxPrice, float MinRating, int NRatings, List<string> Tags);
+
+
+
+public class GetProducts: Endpoint<GetProductsRequest, List<GetProductResponse>>
 {
+    
+    private readonly DataBase _context;
 
-    public class GetProducts : EndpointWithoutRequest<List<GetProductResponse>>
+    public GetProducts(DataBase context)
     {
-        private readonly DataBase _context;
-
-        public GetProducts(DataBase context)
+        _context = context;
+    }
+    
+    public override void Configure()
+    {
+        Post("/products");
+        AllowAnonymous(); // Cambia esto según tu necesidad de autenticación
+    }
+    public override async Task HandleAsync(GetProductsRequest req, CancellationToken ct)
+    {
+        try
         {
-            _context = context;
-        }
 
-        public override void Configure()
-        {
-            Get("/products");
-            AllowAnonymous(); // Cambia esto según tu necesidad de autenticación
-        }
-
-        public override async Task HandleAsync(CancellationToken ct)
-        {
-            try
+            var products = await _context.Products.ToListAsync(ct);
+            
+            if (req.Name != "" || req.Tags.Count > 0  || req.MinPrice >= 0 || req.MaxPrice >= 0 || req.MinRating >=0 ||req.NRatings >= 0)
             {
-                var products = await _context.Products.ToListAsync(ct);
-                var response = products.Select(p => new GetProductResponse
+                if (req.Name != "")
                 {
-                    Id = p.Id,
-                    Name = p.Name,
-                    Price = p.Price,
-                    Image = p.Image,
-                    Tags = p.Tags,
-                }).ToList();
+                    products = products.Where(p => p.Name.ToLower().Contains(req.Name.ToLower())).ToList();
+                }
 
-                await SendAsync(response, 200, ct);
+                if (req.MinPrice >= 0)
+                {
+                    products = products.Where(p => p.Price >= req.MinPrice).ToList();
+                }
+
+                
+                if (req.MaxPrice > 0)
+                {
+                    products = products.Where(p => p.Price <= req.MaxPrice).ToList();
+                }
+
+                if (req.MinRating >= 0)
+                {
+                    products = products.Where(p => p.Rating >= req.MinRating).ToList();
+                }
+
+                
+                if (req.NRatings >= 0)
+                {
+                    products = products.Where(p => p.NRatings >= req.NRatings).ToList();
+                }
+
+                
+                if (req.Tags.Count > 0)
+                {
+                    Console.WriteLine(products.Count);
+                    for (int i = 0; i < req.Tags.Count; i++)
+                    {
+                        Console.WriteLine(req.Tags[i]);
+                        products = products.Where(p => p.Tags.Contains(req.Tags[i])).ToList();
+                    }
+                    Console.WriteLine(products.Count);
+                }
+
             }
-            catch (Exception ex)
+            var response = products.Select(p=>new GetProductResponse
             {
-                await SendErrorsAsync(500, ct);
-            }
+                Id = p.Id,
+                Name = p.Name,
+                Price = p.Price,
+                Image = p.Image,
+                Tags = p.Tags,
+            }).ToList();
+            
+            await SendAsync(response, 200, ct);
+        }
+        catch (Exception ex)
+        {
+            await SendErrorsAsync(500, ct);
         }
     }
 }
